56eb82abc0caf24a079c52cb8c816be3b5d1e3e6
LanFly <1260022720@qq.com>
2019-04-29 02:07:03 +0800

    upload image to qiniu cdn

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,6 +4,6 @@ sh ./build/replace-qiniu.sh
 gitbook build
 git checkout ./
 chmod -R 775 ./*
-#sh ./build/upload-qiniu.sh
+sh ./build/upload-qiniu.sh
 #sh ./build/replace-qiniu.sh
 sh ./build/cp-website.sh
